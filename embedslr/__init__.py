ff2a4702f0ed3c4e99ac3ec9521a9b77ce8ff608
s-matysik <158293088+s-matysik@users.noreply.github.com>
2025-07-30 00:03:29 +0200

    Update __init__.py

--- embedslr/__init__.py
+++ embedslr/__init__.py
@@ -1,18 +1,19 @@
 from importlib import metadata as _meta
 from .embeddings import get_embeddings, list_models
 from .similarity import rank_by_cosine
-from .bibliometrics import full_report
+from .bibliometrics import full_report, indicators
 from .colab_app import run as colab_run
 
 try:
     __version__ = _meta.version(__name__)
 except _meta.PackageNotFoundError:
-    from ._version import __version__  # noqa: F401
+    __version__ = "0.4.0"
 
 __all__ = [
     "get_embeddings",
     "list_models",
     "rank_by_cosine",
     "full_report",
+    "indicators",
     "colab_run",
 ]
